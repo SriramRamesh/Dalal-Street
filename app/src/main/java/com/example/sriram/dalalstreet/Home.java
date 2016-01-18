614c19282c63bf20877d3b5d777f745b5cb8d37a
Sriram Ramesh <grksriram321@gmail.com>
2016-01-18 14:59:15 +0530

    Added home activity and Image slider

--- app/src/main/java/com/example/sriram/dalalstreet/Home.java
+++ app/src/main/java/com/example/sriram/dalalstreet/Home.java
@@ -1,101 +1,101 @@
 package com.example.sriram.dalalstreet;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class Home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Transformers.BaseTransformer;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.HashMap;
+
+import javax.xml.transform.Transformer;
+
+public class Home extends Activity {
+    private SliderLayout sliderShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        sliderShow = (SliderLayout) findViewById(R.id.slider);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextSliderView textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("LG reports quaterly loss in revenue")
+                .image(R.drawable.lg2);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        sliderShow.addSlider(textSliderView);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("Sony acquires assets in China")
+                .image(R.drawable.sony4);
+
+        sliderShow.addSlider(textSliderView);
+        textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("Infy set to invest 120 million on latest tech")
+                .image(R.drawable.infy1);
+
+        sliderShow.addSlider(textSliderView);
+        textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("Github employees calls for a strike")
+                .image(R.drawable.github2);
+
+        sliderShow.addSlider(textSliderView);
+        textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("Yahoo reports quaterly loss in revenue")
+                .image(R.drawable.yahoo2);
+
+        sliderShow.addSlider(textSliderView);
+        textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("Apple set to invest 40 million on latest tech")
+                .image(R.drawable.apple1);
+
+        sliderShow.addSlider(textSliderView);
+        textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("HDFC set to invest 75 million on latest tech")
+                .image(R.drawable.hdfc1);
+
+        sliderShow.addSlider(textSliderView);
+
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    public void Play_Dalal(View v){
+        Intent intent=new Intent(Home.this,play_Dalal.class);
+        startActivity(intent);
+        return;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
+    public void Show_Contact(View v){
+        Toast.makeText(getApplicationContext(), "contact :", Toast.LENGTH_SHORT);
+        return;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void Show_Manual(View v){
+        Toast.makeText(getApplicationContext(), "Manual:", Toast.LENGTH_SHORT);
+        return;
     }
-
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    protected void onStop() {
+        sliderShow.stopAutoCycle();
+        super.onStop();
     }
 }
--- app/src/main/java/com/example/sriram/dalalstreet/LoginActivity.java
+++ app/src/main/java/com/example/sriram/dalalstreet/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.sriram.dalalstreet;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -331,7 +332,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+              Intent intent=new Intent(LoginActivity.this,Home.class);
+              startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
